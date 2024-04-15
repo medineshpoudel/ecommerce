4e2654370a311406a24217a0abfabdbd948d0486
medineshpoudel <dineshpoudel58@gmail.com>
2024-04-15 17:34:29 +0545

    Add route for posting product.

---
+++ server/controllers/product/productController.js
@@ -0,0 +1,31 @@
+const Product = require("../../model/Product");
+const postProductController = async (req, res) => {
+  const {
+    title,
+    actualPrice,
+    discountedPrice,
+    productType,
+    image_1,
+    image_2,
+    image_3,
+  } = req.body;
+  if (
+    !title ||
+    !actualPrice ||
+    !discountedPrice ||
+    !productType ||
+    !image_1 ||
+    !image_2 ||
+    !image_3
+  ) {
+    res.status(400).json({ error: "Please fill up all the required fields." });
+  }
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(500).json({ error: error });
+  }
+};
+
+module.exports = { postProductController };
--- server/routes/product.js
+++ server/routes/product.js
@@ -2,12 +2,15 @@ const express = require("express");
 const requireAuth = require("../middlewares/requireAuth");
 const { allowedRoles } = require("../middlewares/roleAuth");
 const router = express.Router();
+const {
+  postProductController,
+} = require("../controllers/product/productController");
 
 router.use(requireAuth);
 router.get("/", (req, res) => {
   res.send("product");
 });
-router.post("/", allowedRoles(["admin"]), (req, res) => {});
+router.post("/", allowedRoles(["admin"]), postProductController);
 router.get("/best-selling", () => {});
 router.get("/featured-product", () => {});
 
