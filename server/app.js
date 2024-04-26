e953be2793533c4aa8244e48be0c87793f1cb646
BishalChhhetri <kcbisal1@gmail.com>
2024-04-26 09:18:36 +0545

    fix: cors error

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ const corsOptions = {
 
     if (
       origin === "http://127.0.0.1:3000" ||
+      origin === "http://localhost:3000" ||
       origin === "http://localhost:8000"
     ) {
       callback(null, true);
