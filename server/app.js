2accc24f2af91fb2b61894519aaa045fac0d1b77
BishalChhhetri <kcbisal1@gmail.com>
2024-05-10 08:35:30 +0545

    fix: cors

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "https://godam-backend.onrender.com",
       "http://127.0.0.1:3000",
       "http://localhost:8000",
       "http://localhost:3000",
