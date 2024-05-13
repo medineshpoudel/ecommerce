4ff498f6ffeb3c7133e50de744ade18e7a697173
medineshpoudel <dineshpoudel58@gmail.com>
2024-05-13 09:32:45 +0545

    Remove server and move it to another repo.

--- server/routes/user/productVendor.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {
-  getProductVendor,
-} = require("../../controllers/product/productOrderController");
-const router = express.Router();
-
-router.get("/product-vendor/:id", getProductVendor);
-
-module.exports = router;
